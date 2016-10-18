71112749aa1554b6e981fc40c6c272e1b02b129b
Jeff Kluge <jeffkl@microsoft.com>
2016-10-18 15:09:28 -0700

    Add build script

---
+++ build.cmd
@@ -0,0 +1,17 @@
+@ECHO OFF
+SETLOCAL
+
+FOR /f "delims=" %%i in ('git rev-list --count HEAD') DO SET COMMITCOUNT=00%%i
+SET VERSION_SUFFIX=%COMMITCOUNT:~-3%
+
+dotnet restore
+IF ERRORLEVEL 1 EXIT /B %ERRORLEVEL%
+
+dotnet build **\project.json
+IF ERRORLEVEL 1 EXIT /B %ERRORLEVEL%
+
+dotnet test src\Microsoft.Build.CommandLine.Arguments.UnitTests\project.json
+IF ERRORLEVEL 1 EXIT /B %ERRORLEVEL%
+
+dotnet pack src\Microsoft.Build.CommandLine.Arguments\project.json --version-suffix=%VERSION_SUFFIX%
+IF ERRORLEVEL 1 EXIT /B %ERRORLEVEL%
